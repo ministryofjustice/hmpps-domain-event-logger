4d818ae26059d31e38b6338ead019375425109c8
Peter Phillips <peter@greenthistle.co.uk>
2023-05-22 15:45:09 +0100

    SDIT-822: ⬆️ Upgrade to latest

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.1.4-beta-4"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.2.0-beta"
   kotlin("plugin.spring") version "1.8.21"
 }
 
@@ -16,15 +16,15 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-security")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
   implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:2.0.0-beta-15")
-  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.0")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:2.0.0")
+  implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.15.1")
   implementation("org.apache.commons:commons-lang3:3.12.0")
-  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:1.25.0")
+  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:1.26.0")
 
-  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.13")
+  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.14")
   testImplementation("com.github.tomakehurst:wiremock-jre8-standalone:2.35.0")
   testImplementation("org.mockito:mockito-inline:5.2.0")
-  testImplementation("org.testcontainers:localstack:1.18.0")
+  testImplementation("org.testcontainers:localstack:1.18.1")
   testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
 }
 
