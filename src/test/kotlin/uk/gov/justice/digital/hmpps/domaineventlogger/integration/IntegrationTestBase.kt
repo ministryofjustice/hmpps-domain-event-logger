09445ffcbd2b508ab5e668eef57a4f4724bd0b88
Steve Rendell <32732937+steverendell@users.noreply.github.com>
2023-10-09 13:34:30 +0100

    SDIT-1128 Upgrade localstack in SQS lib (#52)
    * SDIT-1128 Upgrade localstack in SQS lib
    
    * SDIT-1128 Upgrade localstack in SQS lib
    
    ---------
    
    Co-authored-by: steverendell <steve.rendell@syscon.net>

--- src/test/kotlin/uk/gov/justice/digital/hmpps/domaineventlogger/integration/IntegrationTestBase.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/domaineventlogger/integration/IntegrationTestBase.kt
@@ -4,13 +4,24 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
 import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.DynamicPropertyRegistry
+import org.springframework.test.context.DynamicPropertySource
 import org.springframework.test.web.reactive.server.WebTestClient
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 @ActiveProfiles("test")
 abstract class IntegrationTestBase {
 
-  @Suppress("SpringJavaInjectionPointsAutowiringInspection")
   @Autowired
   lateinit var webTestClient: WebTestClient
+
+  companion object {
+    private val localStackContainer = LocalStackContainer.instance
+
+    @JvmStatic
+    @DynamicPropertySource
+    fun testcontainers(registry: DynamicPropertyRegistry) {
+      localStackContainer?.also { LocalStackContainer.setLocalStackProperties(it, registry) }
+    }
+  }
 }
