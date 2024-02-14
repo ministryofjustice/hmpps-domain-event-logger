18a002927233db65e72aa78a1203a5f7477fbd76
Peter Phillips <peter@greenthistle.co.uk>
2024-02-14 09:21:55 +0000

    SDIT-1500: 🔒️ Upgrade to latest plugin to suppress nimbus JWT vuln

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.15.1"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.15.2"
   kotlin("plugin.spring") version "1.9.22"
 }
 
@@ -12,17 +12,17 @@ repositories {
   mavenCentral()
 }
 dependencies {
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:0.0.5")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:0.0.6")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
 
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:2.2.1")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:3.1.1")
   implementation("org.apache.commons:commons-lang3:3.14.0")
   implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:1.32.0")
 
   testImplementation("io.swagger.parser.v3:swagger-parser:2.1.20")
   testImplementation("org.wiremock:wiremock:3.3.1")
   testImplementation("org.mockito:mockito-inline:5.2.0")
-  testImplementation("org.testcontainers:localstack:1.19.3")
+  testImplementation("org.testcontainers:localstack:1.19.5")
   testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
 }
 
