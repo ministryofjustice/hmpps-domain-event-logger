c5e031271454a0309c51dc98b9ad8eaff18e2a98
Mike Halma <mike.halma@digital.justice.gov.uk>
2024-02-27 17:14:54 +0000

    SDIT-1336 Use latest hmpps-kotlin-spring-boot-starter lib

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ repositories {
   mavenCentral()
 }
 dependencies {
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:0.1.2")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:0.2.1")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
 
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:3.1.1")
