7375b4d96e428fa39557e959bbfad751f5bd67f2
petergphillips <5099053+petergphillips@users.noreply.github.com>
2024-03-22 15:52:46 +0000

    SDIT-1616: ⬆️ Upgrade to latest spring boot (#81)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.15.3"
-  kotlin("plugin.spring") version "1.9.22"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.15.4"
+  kotlin("plugin.spring") version "1.9.23"
 }
 
 configurations {
@@ -17,7 +17,7 @@ dependencies {
 
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:3.1.1")
   implementation("org.apache.commons:commons-lang3:3.14.0")
-  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:1.32.0")
+  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:1.33.1")
 
   testImplementation("io.swagger.parser.v3:swagger-parser:2.1.21")
   testImplementation("org.wiremock:wiremock:3.4.2")
