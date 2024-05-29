4de862f8bfe6836783da1ce1b182933b460b7248
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2024-05-29 14:51:53 +0100

    SDIT-1788: ➖ Remove unnecessary repositories section

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,10 +7,6 @@ configurations {
   testImplementation { exclude(group = "org.junit.vintage") }
 }
 
-repositories {
-  maven { url = uri("https://repo.spring.io/milestone") }
-  mavenCentral()
-}
 dependencies {
   implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.0.0")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
