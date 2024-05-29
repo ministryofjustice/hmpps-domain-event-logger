a6f3cb8fa4d46ea3f402f0ef3165b6d218f93e53
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2024-05-29 11:23:18 +0100

    SDIT-1788: ⬆️ Upgrade to latest kotlin lib

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,10 +12,10 @@ repositories {
   mavenCentral()
 }
 dependencies {
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:0.2.4")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.0.0")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
 
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:3.1.3")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:4.0.0")
   implementation("org.apache.commons:commons-lang3:3.14.0")
   implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:1.33.3")
 
