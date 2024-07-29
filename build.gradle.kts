c7edbdc6157f9223e4b9046431235aed409f7acd
Mike Halma <mike.halma@digital.justice.gov.uk>
2024-07-29 08:58:31 +0100

    SDIT-1919 Try out Gradle Spring Boot plugin version 6.0.2-beta

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.1"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.2-beta"
   kotlin("plugin.spring") version "2.0.0"
 }
 
@@ -12,13 +12,13 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
 
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:4.1.0")
-  implementation("org.apache.commons:commons-lang3:3.14.0")
-  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:2.4.0")
+  implementation("org.apache.commons:commons-lang3:3.15.0")
+  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:2.6.0")
 
   testImplementation("io.swagger.parser.v3:swagger-parser:2.1.22")
-  testImplementation("org.wiremock:wiremock:3.7.0")
+  testImplementation("org.wiremock:wiremock:3.9.1")
   testImplementation("org.mockito:mockito-inline:5.2.0")
-  testImplementation("org.testcontainers:localstack:1.19.8")
+  testImplementation("org.testcontainers:localstack:1.20.0")
   testImplementation("org.awaitility:awaitility-kotlin:4.2.1")
 }
 
