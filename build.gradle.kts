9e08f84eb32dcdd60e269890dc7896151a0e4b62
petergphillips <5099053+petergphillips@users.noreply.github.com>
2024-10-25 08:20:08 +0100

    SDIT-2141: ⬆️ Upgrade to hmpps spring boot 6.0.8 for path directory traversal suppression (#112)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.7"
-  kotlin("plugin.spring") version "2.0.20"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.8"
+  kotlin("plugin.spring") version "2.0.21"
 }
 
 configurations {
@@ -8,17 +8,17 @@ configurations {
 }
 
 dependencies {
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.0.7")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.0.8")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
 
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.0.1")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.0.2")
   implementation("org.apache.commons:commons-lang3:3.17.0")
   implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:2.9.0")
 
-  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.22")
-  testImplementation("org.wiremock:wiremock:3.9.1")
+  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.23")
+  testImplementation("org.wiremock:wiremock:3.9.2")
   testImplementation("org.mockito:mockito-inline:5.2.0")
-  testImplementation("org.testcontainers:localstack:1.20.2")
+  testImplementation("org.testcontainers:localstack:1.20.3")
   testImplementation("org.awaitility:awaitility-kotlin:4.2.2")
 }
 
