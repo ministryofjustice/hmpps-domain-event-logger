2549c7242064b68c1fb91c8db0af2c37739a7716
petergphillips <5099053+petergphillips@users.noreply.github.com>
2024-11-26 08:24:13 +0000

    SDIT-2318: ⬆️  Upgrade spring boot to 3.4.0 (#118)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.0.9"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.1.0"
   kotlin("plugin.spring") version "2.0.21"
 }
 
@@ -8,17 +8,17 @@ configurations {
 }
 
 dependencies {
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.0.8")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.1.0")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
 
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.1.0")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.2.0")
   implementation("org.apache.commons:commons-lang3:3.17.0")
   implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:2.9.0")
 
   testImplementation("io.swagger.parser.v3:swagger-parser:2.1.24")
   testImplementation("org.wiremock:wiremock:3.9.2")
   testImplementation("org.mockito:mockito-inline:5.2.0")
-  testImplementation("org.testcontainers:localstack:1.20.3")
+  testImplementation("org.testcontainers:localstack:1.20.4")
   testImplementation("org.awaitility:awaitility-kotlin:4.2.2")
 }
 
