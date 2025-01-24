8954c48c6cb4909b87d245035aefaf594d8bc3d8
Mike Halma <mike.halma@digital.justice.gov.uk>
2025-01-24 15:08:18 +0000

    SDIT-2488 Upgrade to Spring Boot 3.4.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "6.1.2"
-  kotlin("plugin.spring") version "2.0.21"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "7.0.0-beta"
+  kotlin("plugin.spring") version "2.1.0"
 }
 
 configurations {
@@ -13,9 +13,9 @@ dependencies {
 
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.2.2")
   implementation("org.apache.commons:commons-lang3:3.17.0")
-  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:2.9.0")
+  implementation("io.opentelemetry.instrumentation::opentelemetry-instrumentation-annotations:2.9.0")
 
-  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.24")
+  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.25")
   testImplementation("org.wiremock:wiremock:3.10.0")
   testImplementation("org.mockito:mockito-inline:5.2.0")
   testImplementation("org.testcontainers:localstack:1.20.4")
