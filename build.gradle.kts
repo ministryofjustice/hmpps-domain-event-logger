8c4a8294a0debc129017d654345674ad84508bb4
Mike Halma <mike.halma@digital.justice.gov.uk>
2025-06-12 09:08:00 +0100

    SDIT-2793 Upgrade dependencies and use client tracking from library

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,17 +8,17 @@ configurations {
 }
 
 dependencies {
-  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.4.5")
+  implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:1.4.6")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
 
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.4.5")
   implementation("org.apache.commons:commons-lang3:3.17.0")
-  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:2.14.0")
+  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:2.16.0")
 
-  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.28")
-  testImplementation("org.wiremock:wiremock:3.13.0")
+  testImplementation("io.swagger.parser.v3:swagger-parser:2.1.29")
+  testImplementation("org.wiremock:wiremock:3.13.1")
   testImplementation("org.mockito:mockito-inline:5.2.0")
-  testImplementation("org.testcontainers:localstack:1.21.0")
+  testImplementation("org.testcontainers:localstack:1.21.1")
   testImplementation("org.awaitility:awaitility-kotlin:4.3.0")
 }
 
