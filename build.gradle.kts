10c4c99f9c296dd4458555026f218561ebb02ee3
Peter Phillips <5099053+petergphillips@users.noreply.github.com>
2025-08-13 12:07:47 +0100

    SDIT-2989: ⬆️  Upgrade for app insights agent fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.3.4"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "8.3.5"
   kotlin("plugin.spring") version "2.2.0"
 }
 
@@ -13,7 +13,7 @@ dependencies {
 
   implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:5.4.10")
   implementation("org.apache.commons:commons-lang3:3.18.0")
-  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:2.16.0")
+  implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations:2.18.1")
 
   testImplementation("io.swagger.parser.v3:swagger-parser:2.1.31")
   testImplementation("org.wiremock:wiremock:3.13.1")
