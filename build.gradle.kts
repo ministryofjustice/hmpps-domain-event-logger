a2524e46e905f51fa239bfa5e1118aa4fe0b8bd1
petergphillips <5099053+petergphillips@users.noreply.github.com>
2026-01-05 16:51:52 +0000

    SDIT-3270: ⬆️  Use non beta version of hmpps sqs library too (#183)

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ dependencies {
   implementation("uk.gov.justice.service.hmpps:hmpps-kotlin-spring-boot-starter:2.0.0")
   implementation("org.springframework.boot:spring-boot-starter-webflux")
 
-  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:6.0.0-beta-2")
+  implementation("uk.gov.justice.service.hmpps:hmpps-sqs-spring-boot-starter:6.0.0")
   implementation("org.apache.commons:commons-lang3:3.20.0")
   // Needs to match this version https://github.com/microsoft/ApplicationInsights-Java/blob/<version>/dependencyManagement/build.gradle.kts#L16
   // where <version> is the version of application insights pulled in by hmpps-gradle-spring-boot
